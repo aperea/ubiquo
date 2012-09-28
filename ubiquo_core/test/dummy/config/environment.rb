6afb8fd2cafc9e3241d599b8d605850f811f8aa1
Alejandro Perea <aperea@gnuine.com>
2012-09-28 16:52:45 +0200

    Dummy Application initialize after database creation

--- ubiquo_core/test/dummy/config/environment.rb
+++ ubiquo_core/test/dummy/config/environment.rb
@@ -1,10 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-Dummy::Application.initialize!
-
 Dummy::Application.load_tasks
 Rake::Task['ubiquo:install'].invoke
 Rake::Task['db:create'].invoke
 ActiveRecord::Migrator.migrate File.expand_path("../db/migrate/", __FILE__)
+
+Dummy::Application.initialize!
